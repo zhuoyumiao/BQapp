fa0d10c4f793cb9e94efaf890012306df76db013
缪卓妤 <12629065+miao-zhuoyu@user.noreply.gitee.com>
2025-11-06 20:16:00 -0500

    deploy

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,13 +1,22 @@
 // client/vite.config.js
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { resolve } from 'path';
 
 export default defineConfig({
   plugins: [react()],
   server: {
     port: 5174,
     proxy: {
-      "/api": { target: "http://localhost:3000", changeOrigin: true },
+      '/api': { target: 'http://localhost:3000', changeOrigin: true },
+    },
+  },
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        instruction: resolve(__dirname, 'instruction.html'),
+      },
     },
   },
 });
