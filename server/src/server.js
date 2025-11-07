c272474ddd7b11529473f5286ca4d45b61d27b8c
缪卓妤 <12629065+miao-zhuoyu@user.noreply.gitee.com>
2025-11-06 19:35:37 -0500

    deploy

--- server/src/server.js
+++ server/src/server.js
@@ -21,7 +21,7 @@ const PORT = process.env.PORT || 3000;
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, '../client/dist')));
 
 app.use(express.json());
 
